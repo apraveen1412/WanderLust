1743b246c30d7f84166b034800210feaae8f1d96
apraveen1412 <aaraveetipraveen@gmail.com>
2026-07-26 14:50:20 +0530

    google uri fix

--- app.js
+++ app.js
@@ -91,7 +91,8 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: process.env.GOOGLE_CALLBACK || "/auth/google/callback",
+            callbackURL: process.env.GOOGLE_CALLBACK,
+            // || "/auth/google/callback", 
         },
         async (accessToken, refreshToken, profile, done) => {
             try {
