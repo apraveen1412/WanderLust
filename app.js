2e36529e8035d3f1c3b54a5eca57d5c929e2a485
apraveen1412 <aaraveetipraveen@gmail.com>
2026-07-27 22:05:51 +0530

    minor fix

--- app.js
+++ app.js
@@ -92,7 +92,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "/auth/google/callback",
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
             // || "/auth/google/callback", 
             // process.env.GOOGLE_CALLBACK_URL
         },
